ccd67a274b215d3a0317f28a13f946df113e3685
weekbelt <vfrvfr4207@gmail.com>
2021-05-21 18:40:32 +0900

    Initial commit

---
+++ src/main/java/me/weekbelt/directorygenerator/DirectoryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.directorygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectoryGeneratorApplication.class, args);
+    }
+
+}
