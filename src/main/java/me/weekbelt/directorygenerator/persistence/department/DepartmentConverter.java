ff60e62f1cd2b0735a10390591aa7c386590a2d8
weekbelt <vfrvfr4207@gmail.com>
2021-06-08 15:23:50 +0900

    동래 new 조직도 추가

--- src/main/java/me/weekbelt/directorygenerator/persistence/department/DepartmentConverter.java
+++ src/main/java/me/weekbelt/directorygenerator/persistence/department/DepartmentConverter.java
@@ -62,7 +62,8 @@ public class DepartmentConverter {
         return optionalParentDept.map(parentDept -> {
             Phone phone = Phone.builder()
                 .number(parentDept.getPhone().getNumber())
-                .type("INWARD_DIALING")
+//                .type("INWARD_DIALING")
+                .type(Objects.requireNonNull(parentDept.getPhone()).getType().equals("내선") ? "INWARD_DIALING" : "OUTWARD_DIALING")
                 .build();
             return NewDepartmentJson.ParentDept.builder()
                 .id(String.valueOf(parentDept.getId()))
